ff124354ee63465d1a5e9b65b89ced2fdae98eb8
igotbitches <check.point9917@yandex.ru>
2021-01-30 23:02:58 +0300

    al test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    newString = str;
+
+    while (newString != "") {
+        flag = false;
+        for (i = 0; i < bracketsConfig.length; i++) {
+            brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+            if (newString.includes(brackets)) {
+                newString = newString.replace(brackets, "");
+                flag = true;
+            }
+        }
+        if (!flag) break;
+    }
+    if (newString === "") return true;
+    else return false;
+};
